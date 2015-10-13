cad2f12cd5be1a2a67d2508fb44ff63662321029
Kacy James <inquen@gmail.com>
2015-10-13 14:40:53 -0400

    Moved prefabs directory

--- Assets/Scripts/RoadSegmentGenerator.cs
+++ Assets/Scripts/RoadSegmentGenerator.cs
@@ -3,17 +3,26 @@ using System.Collections;
 
 public class RoadSegmentGenerator : MonoBehaviour {
 	public GameObject roadSegment;
-	private Vector3 _originalInstantiationPosition;
+	public GameObject intersection;
+	private Vector3 _roadSegmentInstantiationPosition;
+	private Vector3 _intersectionInstantiationPosition;
 	private float _originalInstantiationSize;
 	// Use this for initialization
 	void Start () {
-		_originalInstantiationPosition = roadSegment.transform.position;
+		_intersectionInstantiationPosition = intersection.transform.position;
+		_roadSegmentInstantiationPosition = roadSegment.transform.position;
 		_originalInstantiationSize = GameObject.FindGameObjectWithTag ("OriginalRoadSegment").GetComponent<Renderer>().bounds.size.x;
 	}
-
+	// 15.81, -16.4, -25.5
 	public void Generate() {
-		_originalInstantiationPosition.x += _originalInstantiationSize;
-		Instantiate(roadSegment, _originalInstantiationPosition, Quaternion.identity);
+		int n = Random.Range (1, 10);
+		if (n % 2 == 0) {
+			_roadSegmentInstantiationPosition.x += _originalInstantiationSize;
+			Instantiate (roadSegment, _roadSegmentInstantiationPosition, Quaternion.identity);
+		} else {
+			_intersectionInstantiationPosition.x += _originalInstantiationSize + 4;
+			Instantiate (intersection, _intersectionInstantiationPosition, Quaternion.identity);
+		}
 	}
 	
 	// Update is called once per frame
