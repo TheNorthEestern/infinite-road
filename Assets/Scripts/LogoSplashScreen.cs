2d032fcea6e87ae6e9d9b4fa820662ea54e9e00c
Kacy James <inquen@gmail.com>
2015-10-15 14:51:26 -0400

    Made a swath of changes including adding title screen

---
+++ Assets/Scripts/LogoSplashScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoSplashScreen : MonoBehaviour {
+
+	void Start() {
+		StartCoroutine (TitleScreenDuration ());
+	}
+
+	private IEnumerator TitleScreenDuration() {
+		yield return new WaitForSeconds(5);
+		Application.LoadLevel ("hillside_scene");
+	}
+}
---
+++ Assets/Scripts/TitleScreenCameraBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenCameraBehavior : MonoBehaviour {
+	void Awake() {
+
+	}
+	// Use this for initialization
+	void Start () {
+		iTween.CameraFadeAdd();
+		iTween.CameraFadeTo (iTween.Hash ("amount", 1.0f, "time", 2.0f, "delay", 3.0f));
+		iTween.CameraFadeAdd();
+		iTween.CameraFadeTo(iTween.Hash("amount", 1.0f, "time", 2.0f, "delay", 3.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
