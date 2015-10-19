9392ec25cb91145e06f1efb3de6809b7181248d7
Kacy James <inquen@gmail.com>
2015-10-19 18:52:17 -0400

    Added resources for NPCs

---
+++ Assets/Scripts/NpcSpawnerBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcSpawnerBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().enabled = false;
+		GameObject npc = Resources.Load ("Prefabs/npc") as GameObject;
+		Vector3 newTransform = new Vector3(transform.position.x, npc.transform.position.y, transform.position.z);
+		Instantiate (npc, newTransform, npc.transform.localRotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// StartCoroutine ("GenNpc");
+	}
+
+	IEnumerator GenNpc() 
+	{
+		GameObject npc = Resources.Load ("Prefabs/npc") as GameObject;
+		Instantiate (npc, transform.position, npc.transform.localRotation);
+		yield return new WaitForSeconds(3);
+	}
+}
