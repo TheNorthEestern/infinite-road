ccccbd8903f92eccb5dde7d981333682f4281a7a
Kacy James <inquen@gmail.com>
2015-10-22 15:04:59 -0400

    Did a bit of rearranging, added some more assets

---
+++ Assets/Scripts/Behaviors/RoadBehavior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadBehavior : MonoBehaviour {
+	private MeshRenderer _meshRenderer;
+	public Material[] materialList;
+	private int _textureChoice;
+
+	void Start () {		                          
+		_textureChoice = Random.Range (0, 2);
+		_meshRenderer = GetComponent<MeshRenderer> ();
+		_meshRenderer.material = materialList [_textureChoice];
+	}
+}
