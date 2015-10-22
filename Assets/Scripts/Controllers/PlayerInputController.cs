9ea0d9d73a38c4e4b367a15b4cbade2e153f3c66
Kacy James <inquen@gmail.com>
2015-10-21 22:30:41 -0400

    Fixed mobile code and sonic boom sound code

--- Assets/Scripts/Controllers/PlayerInputController.cs
+++ Assets/Scripts/Controllers/PlayerInputController.cs
@@ -4,7 +4,9 @@ using System.Collections;
 
 public class PlayerInputController : MonoBehaviour {
 	private CharacterController _characterController;
+	private AudioSource _audioSource;
 	private Rigidbody _rb;
+	private bool _sonicBoom = false;
 	public float speed = 10.0f;
 	public float gravity = -9.8f;
 	public float rotationalSpeed = 1.0f;
@@ -14,6 +16,7 @@ public class PlayerInputController : MonoBehaviour {
 
 	void Start () 
 	{
+		_audioSource = GetComponent<AudioSource> ();
 		_rb = GetComponent<Rigidbody> ();
 		_rb.freezeRotation = true;
 		_characterController = GetComponent<CharacterController> ();
@@ -22,39 +25,40 @@ public class PlayerInputController : MonoBehaviour {
 	void FixedUpdate() 
 	{
 		float moveVertical = Input.GetAxis ("Vertical");
-		
 		Vector3 movement;
 
 		movement = new Vector3 (moveVertical, 0.0f, 0.0f);
 
 		if (Application.platform == RuntimePlatform.IPhonePlayer) {
-			if ( Input.touchCount > 0 ) {
-
-				movement = Vector3.right;
-
+			if ( Input.touchCount >= 0 ) {
 				switch(Input.touchCount) {
-				case 1:
-					speed = 10;
-					break;
-					
-				case 2:
-					speed = -10;
-					break;
-					
-				default:
-					break;
-				}
-			} 
-			if ( Input.touchCount == 0 ) {
-				movement = Vector3.zero;
-			}
+					case 0:
+						movement = Vector3.zero;
+						moveVertical = -1.0f;
+						break;
 
+					case 1:
+						movement = Vector3.right;
+						break;
+					
+					default:
+						break;
+				}
+			}
 		}
 
 		if (moveVertical < 0) {
 			_rb.drag += .7f;
 		} else {
 			_rb.drag = 0;
+		}
+
+		Debug.Log ("Sonic Boom " + _sonicBoom); 
+		if (_rb.velocity.x >= 70.0f && _sonicBoom == false) {
+			_audioSource.PlayOneShot (_audioSource.clip);
+			_sonicBoom = true;
+		} else if (_rb.velocity.x <= 70.0f) {
+			_sonicBoom = false;
 		}
 
 		_rb.velocity = Vector3.ClampMagnitude (_rb.velocity, 100.0f);
