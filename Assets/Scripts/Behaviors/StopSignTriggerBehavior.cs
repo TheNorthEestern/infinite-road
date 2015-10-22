87528c1467ae036f2d095a86e6294151bba4f524
Kacy James <inquen@gmail.com>
2015-10-22 15:26:16 -0400

    made a few changes

--- Assets/Scripts/Behaviors/StopSignTriggerBehavior.cs
+++ Assets/Scripts/Behaviors/StopSignTriggerBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class StopSignTriggerBehavior : MonoBehaviour {
 	void Start() {
-		GetComponent<Renderer> ().enabled = false;
+		// GetComponent<Renderer> ().enabled = false;
 	}
 	void OnTriggerExit(Collider other) {
 		Messenger.Broadcast (GameEvent.RAN_STOP_SIGN);
