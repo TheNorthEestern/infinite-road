4a18ec9358841ab8b25164c4d36c333115d27dee
Kacy James <inquen@gmail.com>
2015-11-02 17:16:21 -0500

    Added semi model to game

--- Assets/Scripts/Behaviors/NpcSpawnerBehavior.cs
+++ Assets/Scripts/Behaviors/NpcSpawnerBehavior.cs
@@ -2,27 +2,20 @@
 using System.Collections;
 
 public class NpcSpawnerBehavior : MonoBehaviour {
-	private int _npcSpawnerDeterminant;
-	// Use this for initialization
+	// Together with _npcSpawnerSeed, this variable determines
+	// how often an NPC is spawned
+	private int _npcSpawnerDeterminant = 2;
+	private int _npcSpawnerSeed;
+	private string[] npcModelNames = new string[] {"NPCSemi", "NPC"};
+
 	void Start () {
 		GetComponent<Renderer> ().enabled = false;
-		_npcSpawnerDeterminant = Random.Range (1,7);
-		if ( _npcSpawnerDeterminant % 4 == 0 ) {
-			GameObject npc = Resources.Load ("Prefabs/NPC") as GameObject;
+		_npcSpawnerSeed = Random.Range (1,7);
+		if ( _npcSpawnerSeed % _npcSpawnerDeterminant == 0 ) {
+			GameObject npc = Resources.Load ("Prefabs/" + npcModelNames[0]) as GameObject;
 			Vector3 newTransform = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 			Instantiate (npc, newTransform, transform.localRotation);
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		// StartCoroutine ("GenNpc");
-	}
 
-	IEnumerator GenNpc() 
-	{
-		GameObject npc = Resources.Load ("Prefabs/npc") as GameObject;
-		Instantiate (npc, transform.position, npc.transform.localRotation);
-		yield return new WaitForSeconds(3);
-	}
 }
