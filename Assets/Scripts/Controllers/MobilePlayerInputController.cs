8829d1e4815ca8c6787875befc928552556675f3
Kacy James <inquen@gmail.com>
2015-11-23 13:51:29 -0500

    re-added old controls, new call to action graphic

--- Assets/Scripts/Controllers/MobilePlayerInputController.cs
+++ Assets/Scripts/Controllers/MobilePlayerInputController.cs
@@ -19,71 +19,29 @@ public class MobilePlayerInputController : PlayerController {
 		if (Application.platform == RuntimePlatform.IPhonePlayer || 
 		    Application.platform == RuntimePlatform.Android) {
 			base.FixedUpdate();
-			
-			if ( Input.touchCount >= 0 ) {
-				if (Input.touchCount == 0) {
-					movement = Vector3.right;
-					moveVertical = 1.0f;
-				}
-				if (Input.touchCount == 1) {
+
+			movement = Vector3.right;
+			moveVertical  = 1.0f;
+
+			if ( Input.touchCount > 0 ) {
+				// Touch touch = Input.touches[0];
+			   if ( Input.touchCount == 1 ) {
+				foreach  (Touch touch in Input.touches) {
+					if (touch.position.x < (Screen.width / 2) && (touch.position.y < (Screen.height / 2))) {
+						moveHorizontal = -1.0f;
+					} 
 					
-					foreach (Touch touch in Input.touches) {
-						switch (touch.phase) {
-							
-						case TouchPhase.Began:
-							isSwipe = true;
-							fingerStartTime = Time.time;
-							fingerStartPos = touch.position;
-							break;
-							
-						case TouchPhase.Canceled:
-							isSwipe = false;
-							break;
-							
-						case TouchPhase.Moved:
-							float gestureTime = Time.time - fingerStartTime;
-							float gestureDist = (touch.position - fingerStartPos).magnitude;
-							
-							if (isSwipe && gestureTime < maximumSwipeTime && gestureDist > minimumSwipeDistance){
-								Vector2 direction = touch.position - fingerStartPos;
-								Vector2 swipeType = Vector2.zero;
-								
-								if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)){
-									// the swipe is horizontal:
-									swipeType = Vector2.right * Mathf.Sign(direction.x);
-								} else {
-									// the swipe is vertical:
-									swipeType = Vector2.up * Mathf.Sign(direction.y);
-								}
-								
-								if (direction.x < 0.0f && direction.y > 0.0f) {
-									// Diagonal Left
-									moveHorizontal = -1.0f;	
-								}	
-								if (direction.x > 0.0f && direction.y < 0.0f) {
-									// Diagonal Right
-									moveHorizontal = 1.0f;
-								}
-								
-							} 
-							
-							
-							break;
-							
-						case TouchPhase.Ended:
-							StartCoroutine(Reorient());
-							break;
-							
-						default:
-							break;
-							
-						}
+					else if ((touch.position.x > (Screen.width / 2)) && (touch.position.y < (Screen.height / 2))) {
+						moveHorizontal = 1.0f;
 					}
 				}
-				if (Input.touchCount == 2) {
+			  }
+				if ( Input.touchCount == 2 ) {
 					movement = Vector3.zero;
-					moveVertical = -1.0f;
+					moveVertical = 1.0f;
 				}
+			} else {
+				moveHorizontal = 0.0f;
 			}
 			
 		}
