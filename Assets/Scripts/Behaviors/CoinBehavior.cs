4e2c50a1c9e6c2f47eede02acfce2c53980b88ae
Kacy James <inquen@gmail.com>
2015-12-02 13:29:31 -0500

    Added assets

---
+++ Assets/Scripts/Behaviors/CoinBehavior.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinBehavior : MonoBehaviour {
+	void Update () {
+		// transform.Rotate (new Vector3(0, 100, 0) * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Behaviors/PickUpTriggerBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpTriggerBehavior : MonoBehaviour {
+
+	void Start() {
+		GetComponent<Renderer>().enabled = false;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if ( other.CompareTag ("Player") ) {
+			Messenger.Broadcast (GameEvent.RAN_STOP_SIGN, MessengerMode.DONT_REQUIRE_LISTENER);
+			Destroy (transform.parent.gameObject);
+		}
+	}
+}
