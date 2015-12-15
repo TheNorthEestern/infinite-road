de8b03906e8ef8b747f2d9e26ea7ab08381ec5a6
Kacy James <inquen@gmail.com>
2015-12-15 11:56:41 -0500

    Added rudimentary object pooling

---
+++ Assets/Scripts/Behaviors/HighScoreTextBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighScoreTextBehavior : MonoBehaviour {
+
+	void Start () {
+		GetComponent<Text>().text = "Your Score: " + PlayerPrefs.GetFloat ("CurrentScore").ToString ("F")
+									+ "\nHigh Score: " + PlayerPrefs.GetFloat ("highscore").ToString ("F");
+	}
+
+}
